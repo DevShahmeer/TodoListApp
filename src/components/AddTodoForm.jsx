4aa6f33d80254471d926835c3df98d20eec5eda5
Devshahmeer <theshahmeerfsd@gmail.com>
2023-07-25 12:39:27 +0500

    Added error message feature for empty and duplicate todo tasks

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { addTodo } from '../features/todosSlices'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const AddTodoForm = () => {
   const [ newTask, setNewTask ] = useState('');
+  const [error, setError] = useState('')
+  const inputRef = useRef(null);
   const dispatch = useDispatch();
+  const todos = useSelector(state => state.todos.todos);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(newTask.trim() !== '') {
-      dispatch(addTodo({id: Date.now(), text: newTask, complete: false}))
+    if(newTask.trim() === '') {
+      setError('Todo text cannot be empty.')
+      inputRef.current.focus();
+    } else if(todos.some((todo)=> todo.text === newTask)) {
+      setError('Todo text cannot be empty.');
       setNewTask('');
+      inputRef.current.focus();
+    }
+    else {
+      dispatch(addTodo({id: Date.now(), text: newTask, complete: false}));
+      setNewTask('');
+      setError('')
     }
   }
   return (
     <form className="add-todo-form" onSubmit={handleSubmit}>
+      {error && <p className="error-message">{error}</p>}
       <input
         type="text"
         value={newTask}
